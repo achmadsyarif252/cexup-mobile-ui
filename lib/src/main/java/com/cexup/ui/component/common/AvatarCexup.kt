c280d12d4b4cd017e2197d037cc4c342b7ce3a0b
diewa <diewax@gmail.com>
2023-02-15 16:53:59 +0700

    refactor: new design List Doctor
    #102

--- lib/src/main/java/com/cexup/ui/component/common/AvatarCexup.kt
+++ lib/src/main/java/com/cexup/ui/component/common/AvatarCexup.kt
@@ -1,5 +1,6 @@
 package com.cexup.ui.component.common
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
@@ -15,6 +16,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.cexup.ui.R
 import com.cexup.ui.corporate.theme.BlueDarkJade
 import com.cexup.ui.corporate.theme.BlueLightJade
@@ -25,6 +27,7 @@ import com.skydoves.landscapist.coil.CoilImage
 
 @Composable
 fun AvatarCexup(
+    thumb: String = "",
     isWithStatusOnline: Boolean = false,
     sizeAvatar: Dp = 38.dp.from(LocalContext.current),
     sizeDummy: Dp = 17.dp.from(LocalContext.current),
@@ -32,32 +35,52 @@ fun AvatarCexup(
 ){
     val ctx = LocalContext.current
     Box {
-        Box(
-            modifier = Modifier
-                .clip(CircleShape)
-                .size(sizeAvatar)
-                .background(
-                    brush = Brush.verticalGradient(
-                        colors = listOf(
-                            BlueDarkJade,
-                            BlueLightJade
+        if (thumb.isEmpty()){
+            Box(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(sizeAvatar)
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                BlueDarkJade,
+                                BlueLightJade
+                            )
                         )
-                    )
-                ),
-        ) {
-            CoilImage(
+                    ),
+            ) {
+                Image(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .size(sizeDummy),
+                    painter = painterResource(id = R.drawable.ic_profile_dummy),
+                    contentDescription = "PlaceHolder Avatar",
+                    // Crop, Fit, Inside, FillHeight, FillWidth, None
+                    contentScale = ContentScale.Crop,
+                    // shows an image with a circular revealed animation.
+//                    circularReveal = CircularReveal(duration = 250),
+//                    // shows a placeholder ImageBitmap when loading.
+//                    placeHolder = painterResource(id = R.drawable.ic_profile_dummy),
+//                    // shows an error ImageBitmap when the request failed.
+//                    error = painterResource(id = R.drawable.ic_profile_dummy)
+                )
+            }
+        }else {
+            Image(
                 modifier = Modifier
                     .align(Alignment.Center)
-                    .size(sizeDummy),
-                imageModel = painterResource(id = R.drawable.ic_profile_dummy),
+                    .size(sizeAvatar)
+                    .clip(CircleShape),
+                painter = rememberImagePainter(
+                    data = thumb,
+                    builder = {
+                        crossfade(true)
+                        placeholder(R.drawable.ic_profile_dummy)
+                        error(R.drawable.ic_profile_dummy)
+                    }),
                 // Crop, Fit, Inside, FillHeight, FillWidth, None
                 contentScale = ContentScale.Crop,
-                // shows an image with a circular revealed animation.
-                circularReveal = CircularReveal(duration = 250),
-                // shows a placeholder ImageBitmap when loading.
-                placeHolder = painterResource(id = R.drawable.ic_profile_dummy),
-                // shows an error ImageBitmap when the request failed.
-                error = painterResource(id = R.drawable.ic_profile_dummy)
+                contentDescription = "",
             )
         }
         if (isWithStatusOnline){
--- lib/src/main/java/com/cexup/ui/corporate/component/CardDoctor.kt
+++ lib/src/main/java/com/cexup/ui/corporate/component/CardDoctor.kt
@@ -1,199 +1,138 @@
 package com.cexup.ui.corporate.component
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.cexup.ui.R
-import com.cexup.ui.corporate.theme.BlackShadow
-import com.cexup.ui.corporate.theme.GreyBorder
+import com.cexup.ui.component.common.AvatarCexup
+import com.cexup.ui.corporate.screen.DataDoctorActive
 import com.cexup.ui.corporate.theme.Heading
-import com.cexup.ui.utils.coloredShadow
+import com.cexup.ui.corporate.theme.MaterialThemeCexup
 import com.cexup.ui.utils.mediaquery.from
-import com.skydoves.landscapist.CircularReveal
-import com.skydoves.landscapist.coil.CoilImage
 
 @Composable
 fun CardDoctors(
     modifier: Modifier = Modifier,
-    doctorName: String = "",
-    specialistName: String = "",
-    thumb: String = "",
-    onClickInstagram: () -> Unit = {},
-    onClickWhatsapp: () -> Unit = {},
-    onClickFacebook: () -> Unit = {},
+    dataDoctor: DataDoctorActive,
+    onSelectDoctor: (userCode: String) -> Unit = {},
 ) {
     val ctx = LocalContext.current
-    Column(
-        modifier = modifier.padding(vertical = 12.dp.from(ctx))
+    Card(
+        shape = RoundedCornerShape(16.dp.from(ctx)),
+        elevation = MaterialThemeCexup.elevation.skim
     ) {
-        Card(
+        Column(
             modifier = Modifier
-                .width(172.dp.from(ctx))
-                .height(201.dp.from(ctx))
-                .coloredShadow(
-                    alpha = 0.06f,
-                    color = BlackShadow.copy(alpha = 0.08f),
-                    shadowRadius = 5.dp.from(ctx),
-                    offsetY = 12.dp.from(ctx)
-                ),
-            shape = RoundedCornerShape(10.dp.from(ctx)),
-            elevation = 0.dp.from(ctx)
+                .height(320.dp.from(ctx))
+                .padding(16.dp.from(ctx)),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
-            Column (
-                modifier = Modifier
-                    .padding(
-                        vertical = 18.dp.from(ctx),
-                        horizontal = 8.dp.from(ctx)
-                    )
+            AvatarCexup(
+                isWithStatusOnline = true,
+                sizeAvatar = 60.dp.from(ctx),
+                sizeDummy = 24.dp.from(ctx),
+                sizeCircleOnline = 14.dp.from(ctx),
+                thumb = dataDoctor.thumb,
+            )
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(4.dp.from(ctx))
             ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    CoilImage(
-                        modifier = modifier
-                            .clip(CircleShape)
-                            .coloredShadow(MaterialTheme.colors.primary)
-                            .width(66.dp.from(ctx))
-                            .height(66.dp.from(ctx))
-                            .align(Alignment.CenterVertically),
-                        imageModel = ImageBitmap.imageResource(R.drawable.dummy_profile_small),
-                        // Crop, Fit, Inside, FillHeight, FillWidth, None
-                        contentScale = ContentScale.Crop,
-                        // shows an image with a circular revealed animation.
-                        circularReveal = CircularReveal(duration = 250),
-                        // shows a placeholder ImageBitmap when loading.
-                        placeHolder = ImageBitmap.imageResource(R.drawable.dummy_profile_small),
-                        // shows an error ImageBitmap when the request failed.
-                        error = ImageBitmap.imageResource(R.drawable.dummy_doctor)
-                    )
-                }
-                Spacer(modifier = Modifier.height(15.dp.from(ctx)))
-                Column(
+                Text(
                     modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    text = dataDoctor.title,
+                    style = MaterialThemeCexup.typography.hh2.copy(
+                        fontWeight = FontWeight.SemiBold
+                    ),
+                    color = MaterialThemeCexup.colors.color.text.textMain,
+                    textAlign = TextAlign.Center,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = dataDoctor.specialty,
+                    style = MaterialThemeCexup.typography.hh4,
+                    textAlign = TextAlign.Center,
+                    color = MaterialThemeCexup.colors.color.text.textSecondary,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(2.dp.from(ctx)),
                 ) {
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        text = doctorName,
-                        style = MaterialTheme.typography.body1.copy(
-                            color = Heading,
-                            fontSize = 16.sp.from(ctx),
-                            fontWeight = FontWeight(600)
-                        ),
-                        textAlign = TextAlign.Center,
-                        maxLines = 2,
-                        overflow = TextOverflow.Ellipsis
+                    Icon(
+                        modifier = Modifier.size(16.dp.from(ctx))
+                            .offset(y = 2.dp.from(ctx)),
+                        painter = painterResource(id = R.drawable.ic_star),
+                        contentDescription = "Icon Star Rating",
+                        tint = MaterialThemeCexup.colors.color.warning.warningMain
                     )
                     Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        text = specialistName,
-                        style = MaterialTheme.typography.body1.copy(
-                            color = Heading,
-                            fontSize = 12.sp.from(ctx),
-                            fontWeight = FontWeight(400)
+                        text = dataDoctor.rating,
+                        style = MaterialThemeCexup.typography.hh4.copy(
+                            fontWeight = FontWeight.Medium
                         ),
-                        textAlign = TextAlign.Center,
-                        maxLines = 2,
-                        overflow = TextOverflow.Ellipsis
+                        color = MaterialThemeCexup.colors.color.text.textMain
+                    )
+                    Text(
+                        text = "(${dataDoctor.countRating})",
+                        style = MaterialThemeCexup.typography.hh4,
+                        color = MaterialThemeCexup.colors.color.text.textInactive
+                    )
+                    Spacer(modifier = Modifier.width(4.dp.from(ctx)))
+                    Icon(
+                        modifier = Modifier.size(18.dp.from(ctx)),
+                        painter = painterResource(id = R.drawable.ic_bag_health),
+                        contentDescription = "Icon Bag Health",
+                        tint = MaterialThemeCexup.colors.palette.neutral.neutral8
+                    )
+                    Text(
+                        text = stringResource(
+                            id = R.string.count_years_exp,
+                            dataDoctor.experienced
+                        ),
+                        style = MaterialThemeCexup.typography.hh4,
+                        color = MaterialThemeCexup.colors.color.text.textSecondary
                     )
                 }
-//                Spacer(modifier = Modifier.height(6.dp.from(ctx)))
-//                Row(
-//                    horizontalArrangement = Arrangement.Center,
-//                    modifier = Modifier.fillMaxWidth()
-//                ) {
-//                    Box(
-//                        Modifier
-//                            .size(27.dp.from(ctx))
-//                            .clip(CircleShape)
-//                            .border(
-//                                BorderStroke(
-//                                    1.dp.from(ctx),
-//                                    GreyBorder
-//                                ), shape = CircleShape
-//                            )
-//                            .background(Color.Transparent)
-//                            .clickable {
-//                                onClickInstagram()
-//                            },
-//                        contentAlignment = Alignment.Center
-//                    ) {
-//                        Icon(
-//                            painter = painterResource(id = R.drawable.ic_instagram),
-//                            contentDescription = "Icon Instagram",
-//                            tint = Heading,
-//                            modifier = Modifier.size(14.dp.from(ctx))
-//                        )
-//                    }
-//                    Spacer(modifier = Modifier.width(10.dp.from(ctx)))
-//                    Box(
-//                        Modifier
-//                            .size(27.dp.from(ctx))
-//                            .clip(CircleShape)
-//                            .border(
-//                                BorderStroke(
-//                                    1.dp.from(ctx),
-//                                    GreyBorder
-//                                ), shape = CircleShape
-//                            )
-//                            .background(Color.Transparent)
-//                            .clickable {
-//                                onClickWhatsapp()
-//                            },
-//                        contentAlignment = Alignment.Center
-//                    ) {
-//                        Icon(
-//                            painter = painterResource(id = R.drawable.ic_whatsapp),
-//                            contentDescription = "Icon Whatsapp",
-//                            tint = Heading,
-//                            modifier = Modifier.size(14.dp.from(ctx))
-//                        )
-//                    }
-//                    Spacer(modifier = Modifier.width(10.dp.from(ctx)))
-//                    Box(
-//                        Modifier
-//                            .size(27.dp.from(ctx))
-//                            .clip(CircleShape)
-//                            .border(BorderStroke(1.dp.from(ctx), GreyBorder), shape = CircleShape)
-//                            .background(Color.Transparent)
-//                            .clickable {
-//                                onClickFacebook()
-//                            },
-//                        contentAlignment = Alignment.Center
-//                    ) {
-//                        Icon(
-//                            painter = painterResource(id = R.drawable.ic_facebook),
-//                            contentDescription = "Icon Facebook",
-//                            tint = Heading,
-//                            modifier = Modifier.size(14.dp.from(ctx))
-//                        )
-//                    }
-//                }
+            }
+            Column(verticalArrangement = Arrangement.spacedBy(12.dp.from(ctx))) {
+                Divider(
+                    thickness = 1.dp.from(ctx),
+                    color = MaterialThemeCexup.colors.color.borderline.borderline1,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = { onSelectDoctor(dataDoctor.userCode) },
+                    shape = RoundedCornerShape(4.dp.from(ctx)),
+                    colors = ButtonDefaults.buttonColors(
+                        MaterialThemeCexup.colors.color.primary.primaryMain
+                    ),
+                    contentPadding = PaddingValues(vertical = 13.dp.from(ctx))
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.book_appointment),
+                        style = MaterialThemeCexup.typography.textButton3.copy(
+                            fontWeight = FontWeight.SemiBold
+                        ),
+                        color = MaterialThemeCexup.colors.palette.neutral.neutral1
+                    )
+                }
             }
         }
     }
